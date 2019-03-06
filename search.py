d4da9344e0e34cd08519b7e1815bf5012a981926
HouJX <houjianxia@chelun.com>
2019-03-06 10:14:48 +0800

    delete output json file

--- search.py
+++ search.py
@@ -15,5 +15,8 @@ t2 = time.time()
 word_data = parser(html)
 t3 = time.time()
 print(F'download cost {t1 - t0} s\tparse cost {t3 - t2}')
+'''
 with open('word-data.json', mode='w', encoding='utf-8') as f:
     json.dump(word_data, f, ensure_ascii=False, indent=2)
+'''
+print(json.dumps(word_data, ensure_ascii=False, indent=4))
